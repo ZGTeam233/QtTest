ea008de3f1efe97e669c2df51e9d4723041aae76
ZGTeam233 <wsnzg6@outlook.com>
2026-06-27 14:40:58 +0800

    -

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,17 @@
+#include <QPushButton>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
-    ui->setupUi(this);
+    ui->setupUi(this); // 执行 UI 翻译过来的 C++ 代码
+
+    // 逻辑层：绑定信号与槽 (Android 里的 setOnClickListener)
+    // 当 myButton 被点击时，弹出一个提示框
+    connect(ui->myButton, &QPushButton::clicked, this, [this]() {
+        QMessageBox::information(this, "提示", "Hello from Android dev!\nUI 和逻辑分离真爽！");
+    });
 }
 
 MainWindow::~MainWindow() {
